402ae9a342ecab7429b2a60084ce34848e2e0d30
K1aymore <klaymorer@protonmail.com>
2023-10-03 18:43:28 -0700

    Clean up hardware Add import thingies

--- server.nix
+++ server.nix
@@ -34,6 +34,25 @@
   ];
 
 
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  #boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+
 
   networking = {
     hostName = "server";
@@ -42,7 +61,7 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 25565 ];
-networking.firewall.allowedUDPPorts = [ 25565 ];
+  networking.firewall.allowedUDPPorts = [ 25565 ];
 
   programs.java.enable = true;
 
--- syncthing/nixcfg.nix
+++ syncthing/nixcfg.nix
@@ -1,7 +1,7 @@
 {
   services.syncthing.settings.folders."NixCfg" = {
-      path = "/synced/Nix/cfg";
-      devices = [ "server" "pc" "laptop" "portable" "pinephone" "pixel" "winpc" "acer" ];
-      ignorePerms = false;
-    };
+    path = "/synced/Nix/cfg";
+    devices = [ "server" "pc" "laptop" "portable" "pinephone" "pixel" "winpc" "acer" ];
+    ignorePerms = false;
+  };
 }
