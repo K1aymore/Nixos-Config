5d5a67aaf5a27ad4dcde211104be313fce6c249e
K1aymore <klaymorer@protonmail.com>
2024-10-07 00:44:13 -0700

    Fix error

--- services/system/zfs.nix
+++ services/system/zfs.nix
@@ -1,23 +1,6 @@
 { config, pkgs, lib, ... }:
 
 
-let
-  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
-  zfsCompatibleKernelPackages = lib.filterAttrs (
-    name: kernelPackages:
-    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-    && (builtins.tryEval kernelPackages).success
-    && (
-      (!isUnstable && !kernelPackages.zfs.meta.broken)
-      || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
-    )
-  ) pkgs.linuxKernel.packages;
-  latestKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-in
 {
 
   # ZFS support
@@ -28,11 +11,7 @@ in
   boot.kernelParams = [ "nohibernate" ]; # can cause ZFS corruption
   boot.zfs.forceImportRoot = false;
   
-  assertions = [ 
-    {
 
-    }
-  ];
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
 }
