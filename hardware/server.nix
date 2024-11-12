0400f8594d99772eae8dd887b9027fa4a734a510
K1aymore <klaymorer@protonmail.com>
2024-11-12 13:53:59 -0800

    Back to normal server drive

--- hardware/server.nix
+++ hardware/server.nix
@@ -24,7 +24,7 @@
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/F902-8505";
+    device = "/dev/disk/by-uuid/D0AD-6AD9";
     fsType="vfat";
   };
 
@@ -40,7 +40,7 @@
   }; */
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/a4c05ff8-0f49-42f5-b6fc-4d14f1bd10e3";
+    device = "/dev/disk/by-uuid/a1d47570-1639-4aee-bf8e-106abc6d7047";
     fsType = "ext4";
   };
 
--- server.nix
+++ server.nix
@@ -51,7 +51,7 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sdk"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
 
   networking = {
