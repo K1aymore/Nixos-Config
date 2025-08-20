ca04380e3635e3e98c415bf08f6f28f61b674119
K1aymore <klaymorer@protonmail.com>
2025-08-20 12:56:11 -0700

    Update Plasma 6.4.4, laptop syncthing id

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -26,17 +26,17 @@ in
       settings.devices = {
         "server" = { id = "S6R4I6V-STTDG4D-6X45NCM-MQGF4O7-DTYMQH4-S76ZREL-BWHB7XE-SDTALQC"; };
         "pc" = { id = "AT7GLBF-ZC7D5FF-4JZZ7XO-VWXM5KJ-CCERYXK-6UURHEF-UZWQ5GZ-HCYDDQG"; };
-        "laptop" = { id = "6QZH26V-YJTBBCI-VCEBW4Q-U6ILQA7-PLSWZ4W-YA3KWFQ-KSYTD4A-FS5SPQ7"; };
+        "laptop" = { id = "HWIDO3Z-CKW43X3-X2HDWO5-GQXU6ZD-JVT2QBF-QLXXOHY-4D4LKQ7-ZRDNYQK"; };
         "pixel" = { id = "TM2BIPF-O53YVKR-56UEPPB-E5CU3GC-SX2YXZK-LMQEKG7-F74KCLH-CHFWGAW"; };
         "pixel-5" = { id = "Y4NMW6P-6WFPYZS-27ZSHPP-M4C5I2W-6RODJY3-INLKVLD-TD7MCJO-BBOZAAW"; };
       };
 
-      settings.folders = lib.attrsets.mapAttrs syncFolder {
+      settings.folders = builtins.mapAttrs syncFolder {
         "Archive" = {
           devices = [ "server" "pc" ];
         };
         "NixCfg" = {
-          devices = [ "server" "pc" "laptop" "pixel-5" ];
+          devices = [ "server" "pc" "laptop" "pixel" "pixel-5" ];
           ignorePerms = false;
           path = "/synced/Nix/cfg";
         };
@@ -49,13 +49,13 @@ in
           ignorePerms = true;
         };
         "Media" = {
-          devices = [ "server" "pc" "laptop" "pixel-5" ];
+          devices = [ "server" "pc" "laptop" "pixel" "pixel-5" ];
         };
         "Projects" = {
           devices = [ "server" "pc" "laptop" ];
         };
         "Sync" = {
-          devices = [ "server" "pc" "laptop" "pixel-5" ];
+          devices = [ "server" "pc" "laptop" "pixel" "pixel-5" ];
         };
       };
 
